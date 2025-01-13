1402c32f923895f226bc0307f6e3ef84a3f7bb79
sajjadislam523 <sajjad.islam523@gmail.com>
2025-01-14 02:02:04 +0600

    Added firebase configuration, context API and signin and signup

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,80 @@
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateProfile,
+} from "firebase/auth";
+import { PropTypes } from "prop-types";
+
+import { createContext, useEffect, useState } from "react";
+import { app } from "../firebase/Firebase.config.js";
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
+
+    const signUp = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    };
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    };
+
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo,
+        });
+    };
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+        });
+        return () => {
+            return unsubscribe();
+        };
+    }, []);
+
+    const contextData = {
+        user,
+        loading,
+        signUp,
+        signIn,
+        googleSignIn,
+        logOut,
+        updateUserProfile,
+    };
+    return (
+        <AuthContext.Provider value={contextData}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+AuthProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default AuthProvider;
