ce7c98db0eb03088ede1c8ff3bc25df7c0eb2f6f
sajjadislam523 <sajjad.islam523@gmail.com>
2025-01-21 03:23:31 +0600

    Updated server url

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth.jsx";
 
 const axiosSecure = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://news-sphere-server.vercel.app",
 });
 const useAxiosSecure = () => {
     const navigate = useNavigate();
